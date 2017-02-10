18fc1d8a89c7c2af1589e095506b6075b3ef3977
Gustave F. Gerhardt <gus@thewirecutter.com>
2017-02-10 09:12:24 -0600

    Getting started

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var Button = require('react-button');
     return (
       <div className="App">
         <div className="App-header">
@@ -11,8 +12,14 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To-do app.
         </p>
+        <ul class="todo-items">
+          <li>Create a todo item</li>
+        </ul>
+        
+        <Button color='blue'>Add New Item</Button>
+        
       </div>
     );
   }
